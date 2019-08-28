ac0301528b868333341730db1018bbef459aaeaa
keenechin <keene.chin@gmail.com>
2019-08-28 11:49:26 -0400

    standardized model evaluation deisplay, need better naming scheme

--- code/predict.m
+++ code/predict.m
@@ -2,15 +2,17 @@ function [bias, stdev,y_hat_reg] = predict(regressor,X_test,y_test)
 
 y_hat_reg = regressor.predictFcn(X_test);
 errors = y_hat_reg-y_test;
-err_dist = mean(errors,2);
-bias = mean(err_dist);
-stdev = std(err_dist);
+bias = mean(errors);
+stdev = std(errors);
 figure
-histfit(err_dist);
+histfit(errors);
 xlabel("Position error")
 ylabel("Number of test samples")
+xticks()
+xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'})
 title("Distribution of errors for position classification")
 disp("Prediction bias: "+bias);
 disp("Prediction standard deviation: "+stdev);
-disp("")
+disp("");
+disp("");
 end
